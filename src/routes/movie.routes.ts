9ed48cc1b780e91f594c4387f1f49cecf4eeace1
pemauric <pedro.a.mauricio1@gmail.com>
2023-11-03 20:42:04 -0300

    added middlewares

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -5,12 +5,17 @@ const router = Router();
 //import class 
 import { MovieController } from '../controller/MovieController';
 
+//import validate
+import { validate } from '../middleware/handleValidation';
+import { movieCreateValidation } from '../middleware/movieValidation';
+
 export default router
     .get('/test', (req: Request, res: Response) =>{
         res.status(200).json({message: "API Working"});
     })
-    .post('/movie', MovieController.createMovie)
+    .post('/movie', movieCreateValidation(), validate, MovieController.createMovie)
     .get('/movie/:id', MovieController.getMovieById)
     .get('/movie', MovieController.getAll)
-    .delete('/movie/remove/:id', MovieController.deleteMovieById);
-    .patch('/movie/edit/:id', MovieController.editMovieById);
+    .delete('/movie/remove/:id', MovieController.deleteMovieById)
+    .patch('/movie/:id', movieCreateValidation(), validate, MovieController.editMovieById)
+    
